23ebe1e6f52cf65818e4e03bf083024c2bfcbdf2
mahuanfy <mahuanfy@foxmail.com>
2020-03-13 14:55:09 +0800

    add reduce.

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -6,16 +6,6 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class InverseReduce {
-
-    private Random random = new Random();
-
-    public InverseReduce() {
-    }
-
-    public InverseReduce(Random random) {
-        this.random = random;
-    }
-
     public List<Integer> divideToSmaller(int number) {
         return null;
     }
---
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -0,0 +1,29 @@
+package com.thoughtworks.collection;
+
+import java.util.List;
+
+public class Reduce {
+
+    List<Integer> arrayList;
+
+    public Reduce(List<Integer> arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    public int getMaximum() {
+        return 0;
+    }
+
+    public double getMinimum() {
+        return 0;
+    }
+
+    public double getAverage() {
+        return 0;
+    }
+
+
+    public int getFirstEven() {
+        return 0;
+    }
+}
